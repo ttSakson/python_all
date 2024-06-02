86feba2fca530a52596375e880c0462b830cdb03
Tanya <margaretshtil1@gmail.com>
2024-06-02 23:08:43 +0300

    home work 8.1

---
+++ home_work_8/home_work_8.1.py
@@ -0,0 +1,16 @@
+my_list = [1, 5, 8, 3]
+num_str = ''.join(map(str, my_list))
+
+number = int(num_str)
+
+
+def add_one(number):
+    number += 1
+    return number
+
+
+result_number = add_one(number)
+result_str = str(result_number)
+result_list = [int(char) for char in result_str]
+
+print(result_list)
