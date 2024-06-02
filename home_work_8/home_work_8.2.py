1e48b499dbcfc7330b39e621e202b9d53066f4a1
Tanya <margaretshtil1@gmail.com>
2024-06-03 00:37:15 +0300

    home work

--- home_work_8/home_work_8.2.py
+++ home_work_8/home_work_8.2.py
@@ -1,11 +1,26 @@
+
+import string
+
 def is_palindrome(text):
-    start = 0
-    end = len(text) - 1
+    text = text.lower() #перевести текст у нижній регістр
 
-    while start < end:
-        if text[start] != text[end]:
-            return False
-        start += 1
-        end -= 1
 
-    return True
+    text = ''.join(char for char in text if char.isalnum())
+#'' - видалити всі пробіли
+#join - конкатенція (тобто об'єднання всіх символів в один рядок)
+#цикл пройтись по кожному символу з рядка у тексті
+#char.isalnum() умова перевіряє, чи є кожен символ буквою/цифрою
+
+
+    return text == text[::-1]
+#[::-1] тобто рухаємося у тексті по індексу кожного символа починаючи з кінця і до початку.
+# отже, чи текст == (такий самий/дорівнює) тексту, у якого кроки ідуть з кінця, з останнього індексу
+
+
+# Тестування функції
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
+
